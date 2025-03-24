0fb5d69d3ac682df32fd9a1d349a65017e7a065d
unknown <info.merakinode@gmail.com>
2025-03-24 19:56:51 +0000

    WIP: Local changes before pulling updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
 import About from "./pages/About/About";
 import Service from "./pages/Service/Service";
+import RequestQuote from "./pages/RequestQuote/RequestQuote";
 import "./index.css";
 import React from "react";
 import "./App.css";
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
         <Route path="/service" element={<Service />} />
+        <Route path="/request-quote" element={<RequestQuote />} />
       </Routes>
     </Router>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "/src/assets/home-img/logo.png";
 import Facebook from "/src/assets/home-img/Facebook.png";
 import LinkedIN from "/src/assets/home-img/linkedin-dark.png";
@@ -13,9 +13,20 @@ function Header() {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
+  const closeMobileMenu = (e) => {
+    if (isMobileMenuOpen && !e.target.closest("nav")) {
+      setIsMobileMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", closeMobileMenu);
+    return () => document.removeEventListener("click", closeMobileMenu);
+  }, [isMobileMenuOpen]);
+
   return (
     <header className="bg-body text-white pt-5 pb-4 px-11">
-      <nav className="w-[91.5vw] flex items-center justify-between">
+      <nav className="w-full flex items-center justify-between">
         {/* Logo */}
         <div className="flex items-center">
           <img src={logo} alt="logo" className="h-12 ml-7.5" />
@@ -31,20 +42,21 @@ function Header() {
 
         {/* Navigation Links */}
         <ul
-          className={`absolute md:static top-16 right-6 md:bg-transparent md:flex md:items-center md:space-x-6 transition-all duration-300 ease-in-out z-50 rounded-lg md:rounded-none md:py-0 bg-white ${
-            isMobileMenuOpen ? "block" : "hidden"
-          }`}
+          className={`fixed md:static top-0 right-0 h-[95%] md:h-auto md:bg-transparent md:flex md:items-center md:space-x-6 transition-all duration-300 ease-in-out z-50 md:rounded-none md:py-0 bg-white w-1/2 md:w-auto md:p-0 shadow-lg md:shadow-none transform ${
+            isMobileMenuOpen ? "translate-x-0" : "translate-x-full"
+          } md:translate-x-0`}
         >
-          <div className="md:hidden text-white bg-primary w-full text-center italic py-8">
+          <div className="md:hidden text-white bg-primary w-full text-center italic py-10 mb-6">
             <p>Crafting Excellence</p>
             <p>in Every Byte</p>
           </div>
           <li className="text-center">
-            <li>
-              <Link to="/" className="text-black block py-2 hover:text-primary">
-                Home
-              </Link>
-            </li>
+            <Link
+              to="/"
+              className="text-black block pt-2 pb-2 hover:text-primary"
+            >
+              Home
+            </Link>
           </li>
           <li className="text-center">
             <Link
@@ -63,14 +75,12 @@ function Header() {
             </a>
           </li>
           <li className="text-center">
-            <li>
-              <Link
-                to="/about"
-                className="text-black block py-2 md:py-0 hover:text-primary"
-              >
-                About Us
-              </Link>
-            </li>
+            <Link
+              to="/about"
+              className="text-black block py-2 md:py-0 hover:text-primary"
+            >
+              About Us
+            </Link>
           </li>
           <li className="text-center">
             <a
@@ -82,12 +92,19 @@ function Header() {
           </li>
 
           {/* Request Quote Button */}
-          <button className="bg-primary  text-white px-4 py-2 rounded-lg hover:cursor-pointer">
-            Request a Quote
-          </button>
+          <li>
+            <Link to="/request-quote">
+              <button
+                className="bg-primary text-white px-5 py-2 rounded-lg hover:cursor-pointer 
+                       md:mt-0 md:ml-0 mt-6 ml-30"
+              >
+                Request a Quote
+              </button>
+            </Link>
+          </li>
 
           {/* Social Media Icons (Mobile Only) */}
-          <div className="flex justify-center space-x-4 mt-4 mb-6 md:hidden">
+          <div className="flex justify-center space-x-4 mt-10 mb-6 md:hidden">
             <a href="#">
               <img src={Facebook} alt="Facebook" className="w-6" />
             </a>
