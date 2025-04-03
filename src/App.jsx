acfd3ee31fa65b52dc61ce0b5f291aadc53212b2
Ejiro-Frances <francesejiro@yahoo.com>
2025-04-03 14:29:03 +0100

    removed portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Homepage from "./pages/Homepage/Homepage.jsx";
 import About from "./pages/about/About.jsx";
 import Service from "./pages/Service/Service.jsx";
 import RequestQuote from "./pages/RequestQuote/RequestQuote.jsx";
-import Portfolio from "./pages/Portfolio/Portfolio.jsx";
+// import Portfolio from "./pages/Portfolio/Portfolio.jsx";
 import ProjectCaseStudy from "./pages/ProjectCaseStudy/ProjectCaseStudy.jsx";
 import ContactUs from "./pages/Contact-us/ContactUs.jsx";
 import "./index.css";
@@ -18,7 +18,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/service" element={<Service />} />
         <Route path="/request-quote" element={<RequestQuote />} />
-        <Route path="/portfolio" element={<Portfolio />} />
+        {/* <Route path="/portfolio" element={<Portfolio />} /> */}
         <Route path="/project-case-study/:id" element={<ProjectCaseStudy />} />
         <Route path="/contact" element={<ContactUs />} />
       </Routes>
